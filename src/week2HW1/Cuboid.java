2f5bfdef2e0a477b30e2ab7448941b9991f1f894
Sinderfist <donte_360@yahoo.com>
2021-06-03 09:05:17 -0400

    added notes and cleaned up console logs for grading

--- src/week2HW1/Cuboid.java
+++ src/week2HW1/Cuboid.java
@@ -1,15 +1,21 @@
 package week2HW1;
 
+//Write a class with the name Cuboid that extends Rectangle class. 
 public class Cuboid extends Rectangle{
 
+//	The class needs one field (instance variable) with name height of type double. 
 	double height;
 	
+//	 The class needs to have one constructor with three parameters width, length, and height all of type double.
+//	 It needs to call parent constructor and initialize a height field.	
 	public Cuboid(double length, double width, double height) {
+//		We call super() here to supersede the previous variables in the class of Rectangle that we are extending from
 		super(width, length);
 		
 		this.length = length;
 		this.width = width;
 		
+//		In case the height parameter is less than 0 it needs to set the height field value to 0.
 		if (height <= 0) {
 			height = 0;
 			this.height = height;
@@ -19,21 +25,32 @@ public class Cuboid extends Rectangle{
 		}
 		
 	}
+	
+//	Write the following methods (instance methods):
+	
+	
+//	Method named getHeight without any parameters, it needs to return the value of height field.
 	public double getHeight() {
-		System.out.println(this.height);
 		return this.height;
 		}
 	
+	
+//	Method named getVolume without any parameters, it needs to return the calculated volume.
 	public double getVolume() {
 		this.getArea();
 		this.getHeight();
+		
+//		 To calculate volume multiply the area with height.
 		double Volume = this.getArea() * this.height;
 		return Volume;
 	}
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		
+//		Test Code for main() method
 		Cuboid cuboid = new Cuboid(5,10,5);
 		
 		System.out.println("cuboid width= " + cuboid.getWidth());
--- src/week2HW1/Rectangle.java
+++ src/week2HW1/Rectangle.java
@@ -1,44 +1,55 @@
 package week2HW1;
 
-public class Rectangle {
 
+//Write a class with the name Rectangle. 
+public class Rectangle {
+//	The class needs two fields (instance variable) with name width and length both of type double.
 	double length;
 	double width;
 	
+//	The class needs to have one constructor with parameters width and length both of type double and it needs to initialize the fields.
 	public Rectangle(double W, double L) {
+		
+//		In case the width parameter is less than 0 it needs to set the width field value to 0.
 		if (W <= 0) {
 			W = 0;
 			this.width = W;
-			System.out.println("the Width is " + this.width);
+			System.out.println("the Width is needs to be greater than 0");
 		}	
 		else {
 			this.width = W;
+			
+//		In case the length parameter is less than 0 it needs to set the length field value to 0
 		}
 		if (L <= 0 ) {
 			L = 0;
 			this.length = L;
-			System.out.println("the Length is " + this.length);
+			System.out.println("the Length needs to be greater than 0 ");
 		}
 		else {
 			this.length = L;
 		}
 		
 	}
+//	Write the following methods (instance methods):
 	
+//	Method named getWidth without any parameters, it needs to return the value of width field
 	public double getWidth() {
-		System.out.println(this.width);
+//		System.out.println("The width is " + this.width);
 			return this.width;	
 			
 		}
-	
+//	Method named getLength without any parameters, it needs to return the value of length field
 	public double getLength() {
-			System.out.println(this.length);
+//			System.out.println("The length is " +this.length);
 			return this.length;
 		}
 	
+//	Method named getArea without any parameters, it needs to return the calculated area (width * length).
 	public double getArea() {
-			System.out.println(this.width * this.length);
-			return this.width * this.length;
+		double area = this.width * this.length;
+//			System.out.println("The area is " + area);
+			return area;
 		}
 		
 
@@ -47,10 +58,12 @@ public class Rectangle {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+//		Test Code for main() method
+		
 		Rectangle rectangle = new Rectangle(5,10);
-		System.out.println("rectangle width= " + rectangle.getWidth());
-		System.out.println("rectangle length= " + rectangle.getLength());
-		System.out.println("rectangle area= " + rectangle.getArea());
+		System.out.println(rectangle.getWidth());
+		System.out.println(rectangle.getLength());
+		System.out.println(rectangle.getArea());
 	}
 	
 }
